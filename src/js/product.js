ce2c67ed378e85b364ca9aac71d023a084d25d08
rfmarves <mar23032@byui.edu>
2025-01-11 23:20:03 -0600

    Fixed cart.

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,18 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
+const cartKey = 'so-cart';
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  // gets existing data
+  let cartItems = getLocalStorage(cartKey) || [];
+  
+  // appends new item to list
+  cartItems.push(product);
+  
+  // sets new value to local storage
+  setLocalStorage(cartKey, cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
