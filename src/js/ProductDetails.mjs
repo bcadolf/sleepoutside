282554df5d3873bcbc33b754c8c0a77f86c4576e
anasabinomonteiro <mon23034@byui.edu>
2025-02-04 22:31:12 -0300

    Individual Task week 04- Select Color Product

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-
 import { setLocalStorage, getLocalStorage, alertMessage } from './utils.mjs';
 
 function makeProductCardHtml(product) {
@@ -22,17 +21,48 @@ function makeProductCardHtml(product) {
   return `<section class='product-detail'>
       <h3>${product.Brand.Name}</h3>
       <h2 class='divider'>${product.NameWithoutBrand}</h2>
-      <img class='divider' 
+      
+      <img id='product-image' class='divider product-image' 
       src='${product.Images.PrimaryLarge}'
       alt='${product.NameWithoutBrand}'/>
+
       ${priceHtml}
-      <p class='product-card__price'>$${product.FinalPrice}</p>
-      <p class='product__color'>${product.Colors[0].ColorName}</p>
-      <p class='product__description'>${product.DescriptionHtmlSimple}</p>
+      <p class='product-card_price'>$${product.FinalPrice}</p>
+
+      <div class='product-colors'>
+      <label>Choose a color:</label>
+      <div id="color-options">
+      ${product.Colors.map(color => `
+        <div class='color-option'>
+          <img class='color-chip'
+          src='${color.ColorChipImageSrc}'
+          alt='${color.ColorName}'
+          data-preview='${color.ColorPreviewImageSrc}'
+          title='${color.ColorName}'/>
+        </div>
+      `).join('')}
+      </div>
+      </div>
+      <p class='product__description'>${product.DescriptionHtmlSimple}</p>   
+
       <div class='product-detail__add'>
         <button id='addToCart' data-id='${product.Id}'>Add to Cart</button>
       </div>
     </section>`;
+}
+
+function addColorSelectEvent() {
+  const colorChips = document.querySelectorAll('.color-chip');
+  const productImage = document.getElementById('product-image');
+
+  colorChips.forEach(chip => {
+    chip.addEventListener('click', function() {
+      const newImageSrc = this.getAttribute('data-preview');
+      if (productImage && newImageSrc) {
+        productImage.src = newImageSrc;
+      }
+    });
+  });
 }
 
 export default class ProductDetails {
@@ -50,13 +80,10 @@ export default class ProductDetails {
         return;
       }
       this.product = product;
-      this.renderProductDetails('main');
+      this.renderProductDetails('#product-main');
     } catch (error) {
       console.error('Error initializing product details', error);
     }
-
-    document.getElementById('addToCart').addEventListener('click', this.addToCart.bind(this));
-
   }
 
   addToCart() {
@@ -66,21 +93,14 @@ export default class ProductDetails {
       cart = []; // if cart is empty, create an empty array
     } else if (typeof cart === 'string') {
       try {
-          cart = JSON.parse(cart); // Convert to array if it's a string
-        } catch (error) {
-          console.error('Error parsing cart data', error);
-          cart = [];
-        }     
-    }    
-
-    //Guarantee that cart is an array
-    if (!Array.isArray(cart)) {
-      console.warn('Cart is not an array');
-      cart = [];
+        cart = JSON.parse(cart); // Convert to array if it's a string
+      } catch (error) {
+        console.error('Error parsing cart data', error);
+        cart = [];
+      }
     }
-
     cart.push(this.product);  // Add to cart
-    setLocalStorage('so-cart', cart); //Save in localStorage
+    setLocalStorage('so-cart', cart); // Save in localStorage
     alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
   }
 
@@ -91,6 +111,10 @@ export default class ProductDetails {
       console.error(`Element '${selector}' not found`);
       return;
     }
-    element.insertAdjacentHTML('afterBegin', makeProductCardHtml(this.product));    
+    element.insertAdjacentHTML('afterBegin', makeProductCardHtml(this.product));   
+    
+    document.getElementById('addToCart').addEventListener('click', this.addToCart.bind(this));
+
+    addColorSelectEvent();
   }
 }
